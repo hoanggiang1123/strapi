b62aa5a01774377fed689727dd6df12ffff7c1c0
hoanggiang1123 <hoanggiang1123@gmail.com>
2021-01-07 18:53:03 +0800

    add tags api

--- api/promotion/controllers/promotion.js
+++ api/promotion/controllers/promotion.js
@@ -4,5 +4,33 @@
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
  * to customize this controller
  */
+const { sanitizeEntity } = require('strapi-utils');
 
-module.exports = {};
+module.exports = {
+    async findAll (ctx) {
+
+        let { page, page_size, lang } = ctx.query;
+        let _start = (Number(page) -1) * Number(page_size);
+        let query = {
+            'language.slug': lang,
+            _start,
+            _limit: page_size
+        }
+        let [count, promotions] = await Promise.all([
+            await strapi.services.promotion.count(query),
+            await strapi.services.promotion.find(query)
+        ])
+
+        let data = promotions.map(entity => sanitizeEntity(entity, { model: strapi.models.promotion }));
+        let pagination = {
+            total: Math.ceil( Number(count) / Number(page_size)),
+            page_size: Number(page_size),
+            current_page: Number(page),
+            count: data.length
+        }
+        return {
+            pagination,
+            data
+        }
+    }
+};
--- api/tag/controllers/tag.js
+++ api/tag/controllers/tag.js
@@ -5,4 +5,16 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+    async findAll (ctx) {
+        const { lang } = ctx.query
+        const resp = await strapi.query('tag').find({ 'language': lang });
+        const data = resp.map(item => ({
+            id: item.id,
+            title: item.title,
+            slug: item.slug,
+            language: item.language
+        }));
+        return data;
+    }
+};
